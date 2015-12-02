d3062b9ecc8f55c797ad87201ae2c39c4b04ca1f
Ludvig Eriksson <ludvigeriksson@icloud.com>
2015-12-02 15:44:38 +0100

    1.1.8 release
    Translated to Polish
    New preference bundle header

--- timeuntilalarmprefs/TimeUntilAlarmPrefs.mm
+++ timeuntilalarmprefs/TimeUntilAlarmPrefs.mm
@@ -201,6 +201,65 @@ static const NSBundle *tweakBundle = [NSBundle bundleWithPath:@"/Library/Applica
 
 
 
+@interface PSTableCell : UITableViewCell
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier specifier:(PSSpecifier *)specifier;
+@end
+
+@interface TimeUntilAlarmSettingsHeaderCell : PSTableCell
+@end
+
+@implementation TimeUntilAlarmSettingsHeaderCell {
+	UILabel *_headerLabel;
+	UILabel *_subheaderLabel;
+}
+
+- (id)initWithSpecifier:(PSSpecifier *)specifier {
+	self = [super initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell" specifier:specifier];
+    if (self) {
+		UIFont *font = [UIFont fontWithName:@"HelveticaNeue-Thin" size:40];
+
+		_headerLabel = [[UILabel alloc] init];
+		[_headerLabel setText:@"TimeUntilAlarm"];
+		[_headerLabel setTextColor:[UIColor blackColor]];
+		[_headerLabel setFont:font];
+
+		_subheaderLabel = [[UILabel alloc] init];
+		[_subheaderLabel setText:@"by Ludvig Eriksson"];
+		[_subheaderLabel setTextColor:[UIColor grayColor]];
+		[_subheaderLabel setFont:[font fontWithSize:17]];
+
+		[self addSubview:_headerLabel];
+		[self addSubview:_subheaderLabel];
+    }
+    return self;
+}
+
+- (void)layoutSubviews {
+	[super layoutSubviews];
+
+	[_headerLabel sizeToFit];
+	[_subheaderLabel sizeToFit];
+
+	CGRect frame = _headerLabel.frame;
+	frame.origin.y = 20;
+	frame.origin.x = self.frame.size.width / 2 - _headerLabel.frame.size.width / 2;
+	_headerLabel.frame = frame;
+
+	frame.origin.y += _headerLabel.frame.size.height - 10;
+	frame.origin.x = self.frame.size.width / 2 - _subheaderLabel.frame.size.width / 2;
+	_subheaderLabel.frame = frame;
+}
+
+- (CGFloat)preferredHeightForWidth:(CGFloat)width {
+	// Return a custom cell height.
+	return 80;
+}
+
+@end
+
+
+
+
 
 
 @interface TUATimeFormatListController : PSListController
